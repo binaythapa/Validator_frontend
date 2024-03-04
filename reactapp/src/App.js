a7faa6af18889c294f1992b8492c9adbbf110c44
sabil62 <shresthasabil62@gmail.com>
2024-03-04 12:09:33 +0545

    added tailwind and tailwind macro functionality

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import tw from "twin.macro";
+const Heading = tw.h1`text-red-500 text-2xl p-2`;
+const Container = tw.div`max-w-4xl mx-auto p-5 mt-5`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline bg-orange-300">
+        Hello world!
+      </h1>
+      <Container>
+        <Heading>My custom heading</Heading>
+      </Container>
     </div>
   );
 }
