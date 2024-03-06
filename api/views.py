21623737518c237cb783333dc742bc3ea6ce4e39
sabil62 <shresthasabil62@gmail.com>
2024-03-06 16:23:54 +0545

    Handling exceptions plus get and post request

--- api/views.py
+++ api/views.py
@@ -8,6 +8,9 @@ from django.http import JsonResponse
 from django.views.generic import View
 from .models import Forminfo, Queryinfo
 
+from django.db import transaction
+from django.core.exceptions import ValidationError
+
 
 # def index(request):
 #     return render(request, 'index.html')
@@ -113,41 +116,125 @@ class FormAPIView(APIView):
 
         client_name = data.get("client_name", "")
         client_name_alias = data.get("client_name_alias", "")
-
-        # Extract data for first Model
-        formInfo_data = data.get('api_list', {})
-        print(formInfo_data)
-
-        # Extract second data
         query_info = data.get("query", "")
 
-        query_info_data = {
-            "client_name": client_name,
-            "client_name_alias": client_name_alias,
-            "query": query_info
-        }
+        # Extract data for the first Model
+        formInfo_data = data.get('api_list', {})
+        print("the form data is", formInfo_data)
 
-        query_serializer = QueryInfoSerializer(data=query_info_data)
-        if query_serializer.is_valid():
-            query_serializer.save()
-        else:
-            return Response(query_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            with transaction.atomic():
+                for key in formInfo_data:  # pepco or pound_land
+                    for obj in formInfo_data[key]:
+                        form_data = {
+                            "client_name": client_name,
+                            "client_name_alias": client_name_alias,
+                            "client_file_name": key,
+                            "datatype": obj["datatype"],
+                            "header_name": obj["modified_name"] if obj["modified_name"] != "" else obj["header_name"],
+                            "is_primary_key": obj["is_primary_key"],
+                        }
+                        form_serializer = FormInfoSerializer(data=form_data)
+                        if form_serializer.is_valid():
+                            form_serializer.save()
+                        else:
+                            # Raise an exception to trigger a rollback
+                            raise ValidationError(form_serializer.errors)
+
+                # Extract second data
+                query_info_data = {
+                    "client_name": client_name,
+                    "client_name_alias": client_name_alias,
+                    "query": query_info
+                }
+
+                query_serializer = QueryInfoSerializer(data=query_info_data)
+                if query_serializer.is_valid():
+                    query_serializer.save()
+                else:
+                    # Raise an exception to trigger a rollback
+                    raise ValidationError(query_serializer.errors)
+
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({'message': 'Successfully Processed Data'})
 
-    def get(self, request,  *args, **kwargs):
-        # get all instance
+    def get(self, request, *args, **kwargs):
         formInfo_instance = Forminfo.objects.all()
         queryInfo_instance = Queryinfo.objects.all()
 
-        # serialize data before sending as response
-        formInfo_serializer = FormInfoSerializer(formInfo_instance, many=True)
+        # to convert dbsqlite data to json (serialize)
+        formInfo_serializer = FormInfoSerializer(
+            formInfo_instance, many=True)
         queryInfo_serializer = QueryInfoSerializer(
             queryInfo_instance, many=True)
 
-        # send response
+        # send data
         return Response({
             "formInfo": formInfo_serializer.data,
             "queryInfo": queryInfo_serializer.data
-
         })
+
+
+# class FormAPIView(APIView):
+#     def post(self, request, *args, **kwargs):
+#         data = request.data
+
+#         client_name = data.get("client_name", "")
+#         client_name_alias = data.get("client_name_alias", "")
+
+#         # Extract data for first Model
+#         formInfo_data = data.get('api_list', {})
+
+#         print(formInfo_data)
+
+#         for key in formInfo_data:  # pepco or pound_land
+#             for obj in formInfo_data[key]:
+#                 form_data = {
+#                     "client_name": client_name,
+#                     "client_name_alias": client_name_alias,
+#                     "client_file_name": key,
+#                     "datatype": obj.datatype,
+#                     "header_name": obj.modified_name if obj.modified_name != "" else obj.header_name,
+#                     "is_primary_key": obj.is_primary_key,
+#                 }
+#                 form_serializer = FormInfoSerializer(data=form_data)
+#                 if form_serializer.is_valid():
+#                     form_serializer.save()
+#                 else:
+#                     return Response(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#         # Extract second data
+#         query_info = data.get("query", "")
+
+#         query_info_data = {
+#             "client_name": client_name,
+#             "client_name_alias": client_name_alias,
+#             "query": query_info
+#         }
+
+#         query_serializer = QueryInfoSerializer(data=query_info_data)
+#         if query_serializer.is_valid():
+#             query_serializer.save()
+#         else:
+#             return Response(query_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#         return Response({'message': 'Successfully Processed Data'})
+
+#     def get(self, request,  *args, **kwargs):
+#         # get all instance
+#         formInfo_instance = Forminfo.objects.all()
+#         queryInfo_instance = Queryinfo.objects.all()
+
+#         # serialize data before sending as response
+#         formInfo_serializer = FormInfoSerializer(formInfo_instance, many=True)
+#         queryInfo_serializer = QueryInfoSerializer(
+#             queryInfo_instance, many=True)
+
+#         # send response
+#         return Response({
+#             "formInfo": formInfo_serializer.data,
+#             "queryInfo": queryInfo_serializer.data
+
+#         })
