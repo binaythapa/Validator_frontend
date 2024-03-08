e2723fddfaa269cfbfb8cf3d038519e85df9084a
sabil62 <shresthasabil62@gmail.com>
2024-03-08 13:51:54 +0545

    successfully get and post request

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from .models import Test, File, Client
 from .serializers import TestSerializer, ClientSerializer, FormInfoSerializer, QueryInfoSerializer, FileUploadSerializer
 from django.http import JsonResponse
 from django.views.generic import View
-from .models import Forminfo, Queryinfo
+from .models import Forminfo, Queryinfo, UploadedFile
 
 from django.db import transaction
 from django.core.exceptions import ValidationError
@@ -58,8 +58,19 @@ class FileUploadView(APIView):
             return JsonResponse({'error': 'No file found'}, status=400)
 
 
-class FileUploadView(APIView):
-    def post(self, request, format=None):
+class FileUploadView(generics.ListCreateAPIView):
+    queryset = UploadedFile.objects.all()
+    serializer_class = FileUploadSerializer
+
+    def get_queryset(self):
+        client_name = self.request.query_params.get('client_name', None)
+        if client_name:
+            return UploadedFile.objects.filter(client_name=client_name)
+        # else:
+        #     return UploadedFile.objects.all()
+        return super().get_queryset()
+
+    def post(self, request, *args, **kwargs):
         serializer = FileUploadSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -186,66 +197,3 @@ class FormAPIView(APIView):
             "formInfo": formInfo_serializer.data,
             "queryInfo": queryInfo_serializer.data
         })
-
-
-# class FormAPIView(APIView):
-#     def post(self, request, *args, **kwargs):
-#         data = request.data
-
-#         client_name = data.get("client_name", "")
-#         client_name_alias = data.get("client_name_alias", "")
-
-#         # Extract data for first Model
-#         formInfo_data = data.get('api_list', {})
-
-#         print(formInfo_data)
-
-#         for key in formInfo_data:  # pepco or pound_land
-#             for obj in formInfo_data[key]:
-#                 form_data = {
-#                     "client_name": client_name,
-#                     "client_name_alias": client_name_alias,
-#                     "client_file_name": key,
-#                     "datatype": obj.datatype,
-#                     "header_name": obj.modified_name if obj.modified_name != "" else obj.header_name,
-#                     "is_primary_key": obj.is_primary_key,
-#                 }
-#                 form_serializer = FormInfoSerializer(data=form_data)
-#                 if form_serializer.is_valid():
-#                     form_serializer.save()
-#                 else:
-#                     return Response(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#         # Extract second data
-#         query_info = data.get("query", "")
-
-#         query_info_data = {
-#             "client_name": client_name,
-#             "client_name_alias": client_name_alias,
-#             "query": query_info
-#         }
-
-#         query_serializer = QueryInfoSerializer(data=query_info_data)
-#         if query_serializer.is_valid():
-#             query_serializer.save()
-#         else:
-#             return Response(query_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#         return Response({'message': 'Successfully Processed Data'})
-
-#     def get(self, request,  *args, **kwargs):
-#         # get all instance
-#         formInfo_instance = Forminfo.objects.all()
-#         queryInfo_instance = Queryinfo.objects.all()
-
-#         # serialize data before sending as response
-#         formInfo_serializer = FormInfoSerializer(formInfo_instance, many=True)
-#         queryInfo_serializer = QueryInfoSerializer(
-#             queryInfo_instance, many=True)
-
-#         # send response
-#         return Response({
-#             "formInfo": formInfo_serializer.data,
-#             "queryInfo": queryInfo_serializer.data
-
-#         })
